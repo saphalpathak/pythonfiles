337dc3656e49db11d9038a4a491fb4f2ca19c27c
Saphal <saphalpathak3@gmail.com>
2020-08-11 12:26:59 +0545

    create snake water game with differnt approach

--- universal.py
+++ universal.py
@@ -10,4 +10,22 @@
 # print(function(n))
 # comment
 # This is comments
-
+b = 0
+c = 0
+def what():
+    a = 0
+    while a <10:
+        d = int(input())
+        e = int(input())
+        if d > e:
+            print(f"you won")
+            global b
+            b +=1
+        else:
+            print(f"you loose")
+            global c 
+            c +=1
+        a +=1
+what()
+print(b)
+print(c)
