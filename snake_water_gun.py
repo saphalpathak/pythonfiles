3efd3738c392f9ca8ec8f5e8d6969866ea07359c
Saphal <saphalpathak3@gmail.com>
2020-08-11 11:24:18 +0545

    All done

--- snake_water_gun.py
+++ snake_water_gun.py
@@ -1,61 +1,59 @@
 import random
 def computer():
     list1 = ["snake", "water", "gun"]
-    c_choice = random.choice(list1)
-    # print(c_choice)
+    c_choice =random.choice(list1)
     return c_choice
-def win_loose():
-    choice = computer()
-    print(f"Computer choice: {choice}\nYour choice: {player_input}")
-    if choice == "snake" and player_input == "snake":
-        print("*****Tied*****")
-    elif choice == "snake" and player_input == "water":
-        print("*****You loose*****")
-        ai += 1
-    elif choice == "snake" and player_input == "gun":
-        print("*****You win*****")
-        pl += 1
-    elif choice == "water" and player_input == "snake":
-        print("*****You win*****")
-        pl += 1
-    elif choice == "water" and player_input == "water":
-        print("*****Tied*****")
-    elif choice == "water" and player_input == "gun":
-        print("*****You loose*****")
-        ai += 1
-    elif choice == "gun" and player_input == "snake":
-        print("*****You loose*****")
-        ai += 1
-    elif choice == "gun" and player_input == "water":
-        print("*****You win*****")
-        pl += 1
-    elif choice == "gun" and player_input == "gun":
-        print("*****Tied*****")
+def how_wins():
+    if pl > ai:
+        print(f"Congrats!!!!! You won!\nYour score {pl}\nComputer score {ai}")
+    elif pl < ai:
+        print(f"You Loose!\nYour score {pl}\nComputer score {ai}")
+    else:
+        print("Tied")
+try:
+    chances = 0
+    ai = 0
+    pl = 0
+    while chances <= 9:
+        player_input =input("Please enter your choice snake, water or gun: ")
+        player_input = player_input.lower()
+        choice = computer()
+        if (player_input != "gun" and player_input != "snake" and player_input != "water"):
+            print("Invalid input   Choose between snake or water or gun")
+            continue
+        print(f"Computer choice: {choice}\nYour choice: {player_input}")
+        if choice == "snake" and player_input == "snake":
+            print("*****Tied*****")
+        elif choice == "snake" and player_input == "water":
+            print("*****You loose*****")
+            ai += 1
+        elif choice == "snake" and player_input == "gun":
+            print("*****You win*****")
+            pl += 1
+        elif choice == "water" and player_input == "snake":
+            print("*****You win*****")
+            pl += 1
+        elif choice == "water" and player_input == "water":
+            print("*****Tied*****")
+        elif choice == "water" and player_input == "gun":
+            print("*****You loose*****")
+            ai += 1
+        elif choice == "gun" and player_input == "snake":
+            print("*****You loose*****")
+            ai += 1
+        elif choice == "gun" and player_input == "water":
+            print("*****You win*****")
+            pl += 1
+        elif choice == "gun" and player_input == "gun":
+            print("*****Tied*****")
+        chances +=1
+        print(f"Your score: {pl}")
+        print(f"Computer score: {ai}")
+        print(f"You have {10-chances} left")
+    if chances > 9:
+        print("*******Game Over!*******")
+        how_wins()
 
+except:
+    print("invalid input!")
     
-
-# def how_wins():
-#     if pl > ai:
-#         print(f"You won!\n Your score" {pl} "Computer score" {ai})
-#     elif pl < ai:
-#         print("You Loose!\n Your score" {pl} "Computer score" {ai})
-#     else:
-#         print("Tied")
-
-    
-print("Welcome to snake water gun game!\n You have only 10 chances to win.")
-chances =0
-ai = 0
-pl = 0
-while chances<=9:
-    player_input =input("Please enter your choice snake, water or gun: ")
-    player_input = player_input.lower()
-    win_loose()
-    chances += 1
-    print(ai)
-    print(pl)
-    print(f"You have {10-chances} left")
-if chances >9:
-    print("Game Over!")
-    # how_wins()
-
