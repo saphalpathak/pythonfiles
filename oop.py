2bb254630c6f6175bdd957ee12d3c1ee21d885bc
Saphal <saphalpathak3@gmail.com>
2021-04-02 09:40:55 +0545

    Commit 5
    Signed-off-by: Saphal <saphalpathak3@gmail.com>

--- oop.py
+++ oop.py
@@ -1,18 +1,18 @@
 class Employee():
     no_of_leaves = 10
-    def __init__(self,aname,aage,aposition):
-        self.name = aname
-        self.age = aage
-        self.position = aposition
-    def printdetails(self):
-        return f"Name is {self.name}, age is {self.age} and position is {self.position}"
-    # @classmethod
-    def change_no_of_leaves(clss,newleaves):
-        clss.no_of_leaves = newleaves
-
-
-saphal = Employee("saphal", 20 , "GM")
-rohan = Employee("rohan", 19,"intern")
-saphal.change_no_of_leaves(80)
+    pass
+saphal = Employee()
+rohan = Employee()
+saphal.name = "saphal"
+saphal.age = 27
+saphal.position = "GM"
+rohan.name = "rohan"  
+rohan.age =  23
+rohan.position = "Accountant"
+ 
+Employee.no_of_leaves = 5
 print(Employee.no_of_leaves)
+print(rohan.no_of_leaves)
+rohan.no_of_leaves = 8
+print(rohan.no_of_leaves)
 
